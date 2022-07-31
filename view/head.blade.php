f2a81f0363c9db3d344867edd5d727b705f779dd
redgoose <scripter@me.com>
2022-07-31 15:26:20 +0900

    뷰포트 엘리먼트 부분 조정

--- view/head.blade.php
+++ view/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta name="viewport" content="width=device-width">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="author" content="redgoose-note">
 <meta name="keywords" content="redgoose,redgoose-note,development,graphics,review">
