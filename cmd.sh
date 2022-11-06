f91fc85b364b468bd762a68e166d9d13937f9216
redgoose.eth <scripter@me.com>
2022-11-06 12:12:05 +0900

    nginx reload 부분 추가
    도커만 내렸다 올리면 리버스 프록시 nginx 부분이 꼬이기 때문에 reload 해주게 되었다.

--- cmd.sh
+++ cmd.sh
@@ -4,7 +4,7 @@ case "$1" in
 
   upgrade)
     docker buildx build --platform=linux/amd64 -t redgoose/note.redgoose.me:latest .
-    docker save redgoose/note.redgoose.me:latest | ssh -C goose@redgoose.me 'cd www && docker-compose down && docker load && docker-compose up -d'
+    docker save redgoose/note.redgoose.me:latest | ssh -C goose@redgoose.me 'cd www && docker-compose down && docker load && docker-compose up -d && cd ../service && ./cmd.sh service reload'
     ;;
 
   *)
