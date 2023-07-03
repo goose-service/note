fe4f68ac690a0c0d803ba7a0b813de20b569e279
redgoose.eth <scripter@me.com>
2023-07-03 13:03:51 +0900

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -2,17 +2,22 @@ script="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 case "$1" in
 
+  build)
+    docker buildx build --platform=linux/amd64 -t redgoose/note.redgoose.me:latest .
+    ;;
+
   upload)
-    docker save redgoose/note.redgoose.me:latest | ssh -C goose@redgoose.me 'cd www && docker load'
+    docker save redgoose/note.redgoose.me:latest | ssh -C redgoose.me 'cd ~/docker/note && docker load'
     ;;
 
   upgrade)
     docker buildx build --platform=linux/amd64 -t redgoose/note.redgoose.me:latest .
-    docker save redgoose/note.redgoose.me:latest | ssh -C goose@redgoose.me 'cd www && docker-compose down && docker load && docker-compose up -d && cd ../service && ./cmd.sh service reload'
+    docker save redgoose/note.redgoose.me:latest | ssh -C redgoose.me 'cd ~/docker/note && docker-compose down && docker load && 
+docker-compose up -d && cd ../service && ./cmd.sh service reload'
     ;;
 
   *)
-    echo "Usage: ${script} {upgrade}" >&2
+    echo "Usage: ${script} {build|upload|upgrade}" >&2
     exit 3
     ;;
 
