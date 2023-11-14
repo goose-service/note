b7e5671ec725f21b077718ef95efca9779db1922
redgoose <scripter@me.com>
2023-11-14 16:36:24 +0900

    다크모드 스위치 스타일 조정

--- cmd.sh
+++ cmd.sh
@@ -7,18 +7,19 @@ case "$1" in
     ;;
 
   upload)
-    docker save redgoose/note.redgoose.me:latest | ssh -C redgoose.me 'cd ~/docker/note && docker load'
+    docker save redgoose/note.redgoose.me:latest | ssh -C redgoose.me 'cd ~/docker/note && docker-compose down && docker load &&
+    docker-compose up -d && cd ../service && ./cmd.sh service reload'
     ;;
 
   upgrade)
     docker buildx build --platform=linux/amd64 -t redgoose/note.redgoose.me:latest .
-    docker save redgoose/note.redgoose.me:latest | ssh -C redgoose.me 'cd ~/docker/note && docker-compose down && docker load && 
+    docker save redgoose/note.redgoose.me:latest | ssh -C redgoose.me 'cd ~/docker/note && docker-compose down && docker load &&
 docker-compose up -d && cd ../service && ./cmd.sh service reload'
     ;;
 
   *)
     echo "Usage: ${script} {build|upload|upgrade}" >&2
-    exit 3
+    exit 0
     ;;
 
 esac
